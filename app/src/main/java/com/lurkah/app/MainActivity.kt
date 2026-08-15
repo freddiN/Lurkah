8262ae58264d50f2ef335f5143dbe0b3cba06634
freddi <mr.freddi@proton.me>
2026-08-15 21:38:32 +0200

    fix imports

--- app/src/main/java/com/lurkah/app/MainActivity.kt
+++ app/src/main/java/com/lurkah/app/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
