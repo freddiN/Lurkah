b1a18c76c2faa5355f773e13dd0a9b9bb9b21207
freddi <mr.freddi@proton.me>
2026-08-16 21:00:19 +0200

    gradle 9, image fixes versuch

--- app/src/main/java/com/lurkah/app/MainActivity.kt
+++ app/src/main/java/com/lurkah/app/MainActivity.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import kotlinx.coroutines.delay
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.CachePolicy
